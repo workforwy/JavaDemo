027c4a98f447c7af4c6646d01a320c0c6c580d04
wy <>
2019-08-01 10:34:41 +0800

    ..

--- src/main/java/base/Test3.java
+++ src/main/java/base/Test3.java
@@ -16,12 +16,12 @@ public class Test3 {
         byte b2 = (byte) (n >> 16);
         byte b3 = (byte) (n >> 8);
         byte b4 = (byte) (n >> 0);
-        System.out.println(b4);
-        System.out.println(b3);
-        System.out.println(b2);
         System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
     /*
-     * 0.定义变量r=0
      * 1.b1向左移动24位再不带符号右移0位，再与r求位或，结果重新赋给r
      * 2.b1向左移动24位再不带符号右移8位，再与r求位或，结果重新赋给r
      * 3.b1向左移动24位再不带符号右移16位，再与r求位或，结果重新赋给r
@@ -29,8 +29,11 @@ public class Test3 {
      */
         int r = 0;
         r = r | (b1 << 24 >>> 0);
+        System.out.println(r);
         r = r | (b2 << 24 >>> 8);
+        System.out.println(r);
         r = r | (b3 << 24 >>> 16);
+        System.out.println(r);
         r = r | (b4 << 24 >>> 24);
         System.out.println(r);
     }
